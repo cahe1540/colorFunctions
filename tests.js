d0d3cb0a8fd11bea0ad5e9ecccdab40f762bc0cb
Carlos Herrera <cahe1540@gmail.com>
2021-11-02 23:27:07 -0700

    formatting code

--- tests.js
+++ tests.js
@@ -1,35 +1,56 @@
-const { isColorTooSimilar, hexStringToRgbArray } = require('./colorDistance');
+const { isColorTooSimilar, hexStringToRgbArray } = require("./colorDistance");
 
-console.log('Tests for hexStringToRgbArray');
+console.log("Tests for hexStringToRgbArray");
 /*Tests for functions*/
 /*Test hexStringToRgbArray*/
-let testCases = ['#f4','#f4f4','#f4f4f4','#fff','#000','#0', '#', '', 'f4f4f4', 'f4f'];
-testCases.forEach(color => {
-    try{
-        console.log(hexStringToRgbArray(color));
-    }catch(err){
-        const message = err.message === 'Invalid input, hexColorStr must be of the form "#fff", "#ffffff" or "#f4f4f4"' ? 'pass':'fail';
-        console.log(message);
-    }
+let testCases = [
+  "#f4",
+  "#f4f4",
+  "#f4f4f4",
+  "#fff",
+  "#000",
+  "#0",
+  "#",
+  "",
+  "f4f4f4",
+  "f4f",
+];
+testCases.forEach((color) => {
+  try {
+    console.log(hexStringToRgbArray(color));
+  } catch (err) {
+    const message =
+      err.message ===
+      'Invalid input, hexColorStr must be of the form "#fff", "#ffffff" or "#f4f4f4"'
+        ? "pass"
+        : "fail";
+    console.log(message);
+  }
 });
 
-console.log('\n---\n');
-console.log('Tests for isColorTooSimilar');
+console.log("\n---\n");
+console.log("Tests for isColorTooSimilar");
 
 /*Test isColorTooSimilar*/
 const background = [223, 200, 123];
-testCases = [[224, 201, 123], [223, 200, 123], [255, 255, 255], [0,0,0], [30,30,30]];
+testCases = [
+  [224, 201, 123],
+  [223, 200, 123],
+  [255, 255, 255],
+  [0, 0, 0],
+  [30, 30, 30],
+];
 const assertions = [true, true, false, false, false];
 const threshold = 55;
 
 let i = 0;
-testCases.forEach(color => {
-    try{
-        const result = isColorTooSimilar(background, color, threshold);
-        const message = assertions[i] === result ? 'pass':'fail';
-        console.log(message);
-        i++;
-    }catch(err){
-        console.log('fail');
-    }
+testCases.forEach((color) => {
+  try {
+    const result = isColorTooSimilar(background, color, threshold);
+    const message = assertions[i] === result ? "pass" : "fail";
+    console.log(message);
+    i++;
+  } catch (err) {
+    console.log("fail");
+  }
 });
